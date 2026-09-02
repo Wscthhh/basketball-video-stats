e7361f1eb5821dee7a0ce173260bd670276ab0ce
陈添豪 <wscthhh@163.com>
2026-09-02 10:36:59 +0800

    chore: release v0.1.1

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron')
 const apiArgument = process.argv.find((value) => value.startsWith('--courttrace-api='))
 const mobileArgument = process.argv.find((value) => value.startsWith('--courttrace-mobile='))
 contextBridge.exposeInMainWorld('courtTraceDesktop', {
-  version: '0.1.0',
+  version: '0.1.1',
   apiBase: apiArgument?.slice('--courttrace-api='.length) || '',
   mobileUrl: mobileArgument?.slice('--courttrace-mobile='.length) || '',
   checkForUpdates: () => ipcRenderer.invoke('check-for-updates'),
